d8e4e1f7a6cb22b85cd9e3102caebb9ac66f9327
opeyemibioku <opeyemibioku@gmail.com>
2024-12-03 14:16:03 +0100

    setup the mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { developmentConfig, productionConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '.env'),
+      load:
+        process.env.NODE_ENV === 'production'
+          ? [productionConfig]
+          : [developmentConfig],
+    }),
+    MongooseModule.forRoot(process.env.DEV_MONGODB_CONNECTION_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
