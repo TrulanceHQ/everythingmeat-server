0e1457fa25bf6fe0ea5cb07a2f80c3ad28029b84
Adelolaa <adelola.alesh@gmail.com>
2024-12-11 17:42:00 +0100

    buyer-flow

---
+++ src/products/product.schema.ts
@@ -0,0 +1,7 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from "mongoose";
+@Schema()
+export class Product extends Document {
+
+}
+export const ProductSchema = SchemaFactory.createForClass(Product);
