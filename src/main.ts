9726e338d29f54c59768ddbad96bd536f51b4877
opeyemibioku <opeyemibioku@gmail.com>
2024-12-12 13:54:51 +0100

    setup swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -17,6 +19,15 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT') || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Everything Meat')
+    .setDescription('Everything Meat API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(port);
 
