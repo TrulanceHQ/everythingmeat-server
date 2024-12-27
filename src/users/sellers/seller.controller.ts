f80a3c9dca4d0c0a263ca925293f42ff32787ebf
opeyemibioku <opeyemibioku@gmail.com>
2024-12-27 19:29:22 +0100

    updated the products endpoints for sellers

--- src/users/sellers/seller.controller.ts
+++ src/users/sellers/seller.controller.ts
@@ -26,7 +26,7 @@ import { Roles } from 'src/utils/Roles/roles.decorator';
 
 @Controller('api/v1/seller')
 @ApiTags('Sellers')
-@ApiBearerAuth() // Enables Bearer Token in Swagger UIh
+@ApiBearerAuth() // Enables Bearer Token in Swagger UI
 @UseGuards(RolesGuard)
 export class SellerController {
   constructor(private readonly sellerService: SellerService) {}
