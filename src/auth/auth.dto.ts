51a080497e71d2522e90ef88fd18f581bc5a162f
opeyemibioku <opeyemibioku@gmail.com>
2025-01-23 15:20:35 +0100

    updated the admin endpoints

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -11,7 +11,7 @@ import {
 import { UserRole } from './schema/user.schema';
 
 export class CreateUserDto {
-  @ApiProperty({ description: 'First name of the user', example: 'Test' })
+  @ApiProperty({ description: 'First namme of the user', example: 'Test' })
   @IsString()
   @IsNotEmpty()
   readonly firstName: string;
